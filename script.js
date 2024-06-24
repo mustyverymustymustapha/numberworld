8fdb5e35817d1f940a4600d791dec942f6d14897
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-06-24 11:20:34 +0000

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function getRandomNumber(minRange, maxRange) {
+    return Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+}
+
+window.onload = function() {
+    const rand = getRandomNumber(1, 100);
+    document.getElementById("yourNumber").textContent = rand
+};
+
